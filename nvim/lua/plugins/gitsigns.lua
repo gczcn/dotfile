9652a3a636a0bd3865f4a8b8a5cb8230d41ad216
ZiXuan Chu <gczcnmail@gmail.com>
2024-05-26 23:27:23 +0800

    gitsigns lazyload

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,7 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
-    -- event = 'User FileOpened',
-    lazy = false,
+    event = 'User FileOpened',
     keys = {
       { '<leader>gl', '<cmd>Gitsigns toggle_linehl<CR>', mode = 'n' }
     },
