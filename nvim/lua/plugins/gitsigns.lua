ea5f5f9e8e0bedc8d020f4f7626009f12fb30fa2
ZiXuan Chu <gczcnmail@gmail.com>
2024-05-26 23:20:50 +0800

    go back

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    -- event = 'User FileOpened',
+    lazy = false,
+    keys = {
+      { '<leader>gl', '<cmd>Gitsigns toggle_linehl<CR>', mode = 'n' }
+    },
+    config = function()
+      require('gitsigns').setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 0,
+        },
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+
+          -- stylua: ignore start
+          map('n', ']h', gs.next_hunk, 'Next Hunk')
+          map('n', '[h', gs.prev_hunk, 'Prev Hunk')
+          map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
+          map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
+          map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
+          map('n', '<leader>ghu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+          map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
+          map('n', '<leader>ghp', gs.preview_hunk_inline, 'Preview Hunk Inline')
+          map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
+          map('n', '<leader>ghd', gs.diffthis, 'Diff This')
+          map('n', '<leader>ghD', function() gs.diffthis('~') end, 'Diff This ~')
+          map({ 'o', 'x' }, 'kh', ':<C-U>Gitsigns select_hunk<CR>', 'GitSigns Select Hunk')
+          map('n', '<leader>gl', '<cmd>Gitsigns toggle_linehl<CR>', 'Toggle GitSigns Line Highlight')
+        end,
+      })
+    end
+  }
+}
