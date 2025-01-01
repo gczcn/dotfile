6d51d7f94673952ab812342e7cd28dbe28b118be
Zixuan Chu <gczcnmail@gmail.com>
2025-01-01 20:10:13 +0800

    tmux

---
+++ tmux/.config/tmux-powerline/segments/rainbarf.sh
@@ -0,0 +1,14 @@
+run_segment() {
+    type rainbarf >/dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        echo 'rainbarf was not found'
+        return
+    fi
+
+    # Customize via ~/.rainbarf.conf
+    stats=$(rainbarf --bright --no-battery)
+    if [ -n "$stats" ]; then
+        echo "$stats";
+    fi
+    return 0
+}
