a15c1144331d9de73eeaef4e1619f05500a19faf
gczcn <gczcnmail@gmail.com>
2025-01-29 12:47:59 +0800

    alacritty & nvim: 更改字体到 Blex Mono Nerd Font Mono

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -525,8 +525,8 @@ end
 -- Tags: GUI
 -- =============================================================================
 
-local gui_font = 'Consolas Nerd Font Mono'
-local gui_font_size = 14.5
+local gui_font = 'BlexMono Nerd Font Mono'
+local gui_font_size = 13
 
 local gui_change_font_size = function(n)
   gui_font_size = gui_font_size + n
