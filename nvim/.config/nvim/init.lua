a4af578a5f20d7ac3d8ab6be5a4f8ec8043a8c5d
gczcn <494353540@qq.com>
2025-07-06 16:16:44 +0800

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1456,7 +1456,22 @@ local plugins = global_config.enabled_plugins and {
 			vim.cmd([[
 let g:mkdp_command_for_global = 1
 let g:mkdp_page_title = '"${name}"'
-			]])
+let g:mkdp_theme = 'light'
+let g:mkdp_preview_options = {
+    \ 'mkit': {},
+    \ 'katex': {},
+    \ 'uml': {},
+    \ 'maid': {},
+    \ 'disable_sync_scroll': 0,
+    \ 'sync_scroll_type': 'middle',
+    \ 'hide_yaml_meta': 1,
+    \ 'sequence_diagrams': {},
+    \ 'flowchart_diagrams': {},
+    \ 'content_editable': v:false,
+    \ 'disable_filename': 1,
+    \ 'toc': {}
+    \ }
+]])
 		end,
 	},
 
